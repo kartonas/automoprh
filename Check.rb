11e32c062375e5c5b7b0c9c8cbd8495b734b3455
kartonas <jokubas.mickus@gmail.com>
2016-05-18 21:02:28 +0300

    tikrinimo veikia

--- Check.rb
+++ Check.rb
@@ -1,17 +1,18 @@
 class Check
 
-attr_accessor :number
+  attr_accessor :number
 
-def initialize(number)
-  @number = number
-end
-
-def truefalse()
-  if (number ** 2).to_s.split('').last(number.to_s.size).join.to_i==number
-    puts "valid"
-  else puts "invalid"
+  def initialize(number)
+    @number = number
   end
 
-end
+  def truefalse()
+    if (number ** 2).to_s.split('').last(number.to_s.size).join.to_i==number
+      puts "valid"
+    else
+      puts "invalid"
+    end
+
+  end
 
 end
