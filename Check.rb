eff0a35f81fec04f5b7de948b3e1b5abb41a0a2b
kartonas <jokubas.mickus@gmail.com>
2016-05-18 21:00:45 +0300

    tikrinimo klase veikia

---
+++ Check.rb
@@ -0,0 +1,17 @@
+class Check
+
+attr_accessor :number
+
+def initialize(number)
+  @number = number
+end
+
+def truefalse()
+  if (number ** 2).to_s.split('').last(number.to_s.size).join.to_i==number
+    puts "valid"
+  else puts "invalid"
+  end
+
+end
+
+end
